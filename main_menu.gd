23458fc08caaf37bf455e2089c99b187387ae93a
Kacper Wolski <kacper.wolski.biz@gmail.com>
2026-05-29 17:56:02 +0200

    feat: reset button
    - added button for resetting player statistics (loses, wins and coins) in case the player goes bankrupt and needs to start fresh

--- main_menu.gd
+++ main_menu.gd
@@ -2,3 +2,11 @@ extends Control
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://fighter_select.tscn")
+
+
+func _on_reset_pressed() -> void:
+	$ConfirmationDialog.popup_centered()
+
+
+func _on_confirmation_dialog_confirmed() -> void:
+	PlayerStats.reset_stats()
